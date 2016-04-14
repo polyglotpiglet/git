0854dc812b1dde6b5be2bd9c629a83f29f0ac24b
polyglotpiglet <alexandra.p.batey@gmail.com>
2016-04-14 21:41:28 +0100

    matching push config (old behaviour)

--- .abatey-itv.sh
+++ .abatey-itv.sh
@@ -1,2 +1,3 @@
+git config --global push.default matching
 git config --global user.name "Alexandra Batey"
 git config --global user.email alexandra.batey@itv.com
--- .polyglotpiglet.sh
+++ .polyglotpiglet.sh
@@ -1,2 +1,3 @@
+git config --global push.default matching
 git config --global user.name "polyglotpiglet"
 git config --global user.email alexandra.p.batey@gmail.com
