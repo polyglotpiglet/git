81a9506bf097e72f0512f6719a5a9e038aa10ccd
polyglotpiglet <alexandra.p.batey@gmail.com>
2016-04-14 16:02:04 +0100

    Committing scripts to flip user info between two github accounts

---
+++ .abatey-itv.sh
@@ -0,0 +1,2 @@
+git config --global user.name "Alexandra Batey"
+git config --global user.email alexandra.batey@itv.com
---
+++ .polyglotpiglet.sh
@@ -0,0 +1,2 @@
+git config --global user.name "polyglotpiglet"
+git config --global user.email alexandra.p.batey@gmail.com
